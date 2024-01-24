833b64c7e9844f4a709153277808f753912e8ab6
Rustem Askar <bornqazaqi@gmail.com>
2024-01-24 20:10:16 +0500

    first adding

---
+++ AstanaTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AstanaTap
+//
+//  Created by Rystem Asqar on 1/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
